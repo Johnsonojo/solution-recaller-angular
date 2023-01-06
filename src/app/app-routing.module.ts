2d4b410f39a5408a399af9e9fd2a5525c39a441b
Johnson Ojo <johnsonojo89@gmail.com>
2023-01-06 15:45:55 +0100

    setup bootstrap and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { CreatePostComponent } from './pages/post-page/create-post/create-post.component';
+import { EditPostComponent } from './pages/post-page/edit-post/edit-post.component';
+import { PostPageComponent } from './pages/post-page/post-page.component';
+import { SinglePostComponent } from './pages/post-page/single-post/single-post.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'post', component: PostPageComponent },
+  { path: 'post/:postId', component: SinglePostComponent },
+  { path: 'post/edit-post/:postId', component: EditPostComponent },
+  { path: 'post/create-post', component: CreatePostComponent },
+  { path: 'search', component: SearchPageComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { CreatePostComponent } from './pages/post-page/create-post/create-post.component';
+import { EditPostComponent } from './pages/post-page/edit-post/edit-post.component';
+import { PostPageComponent } from './pages/post-page/post-page.component';
+import { SinglePostComponent } from './pages/post-page/single-post/single-post.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    HomePageComponent,
+    PostPageComponent,
+    SearchPageComponent,
+    NotFoundPageComponent,
+    CreatePostComponent,
+    EditPostComponent,
+    SinglePostComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
