9784ccedf73ac902c6fe137693ecc78eab07a43e
Johnson Ojo <johnsonojo89@gmail.com>
2023-01-09 10:54:46 +0100

    updated all posts page

--- src/app/pages/post-page/post-page.component.ts
+++ src/app/pages/post-page/post-page.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
-  styleUrls: ['./post-page.component.scss']
+  styleUrls: ['./post-page.component.scss'],
 })
 export class PostPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  postData = [
+    {
+      id: 'iDOne',
+      problemTitle: 'Problem Title 1',
+      problemDescription: 'Problem Description 1',
+      problemSolution: 'Problem Solution 1',
+    },
+    {
+      id: 'iDTwo',
+      problemTitle: 'Problem Title 2',
+      problemDescription: 'Problem Description 2',
+      problemSolution: 'Problem Solution 2',
+    },
+    {
+      id: 'iDThree',
+      problemTitle: 'Problem Title 3',
+      problemDescription: 'Problem Description 3',
+      problemSolution: 'Problem Solution 3',
+    },
+    {
+      id: 'iDFour',
+      problemTitle: 'Problem Title 4',
+      problemDescription: 'Problem Description 4',
+      problemSolution: 'Problem Solution 4',
+    },
+    {
+      id: 'iDFive',
+      problemTitle: 'Problem Title 5',
+      problemDescription: 'Problem Description 5',
+      problemSolution: 'Problem Solution 5',
+    },
+  ];
 }
