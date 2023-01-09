73b2a2b33a8649a267c3bacac3f495d21ba298e8
Johnson Ojo <johnsonojo89@gmail.com>
2023-01-09 19:44:07 +0100

    update http service

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -6,14 +6,13 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class HttpService {
-  base_url = environment.base_url;
+  base_url = environment.BASE_URL;
 
   constructor(private http: HttpClient) {}
 
   makeGetRequest(endpoint: string) {
     return this.http.get(`${this.base_url}/${endpoint}`);
   }
-
   makePostRequest(endpoint: string, data: any) {
     return this.http.post(`${this.base_url}/${endpoint}`, data);
   }
