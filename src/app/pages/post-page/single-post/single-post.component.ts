e242b89e2e13142844d87edd81c35ce4cea51bdd
Johnson Ojo <johnsonojo89@gmail.com>
2023-01-12 16:15:09 +0100

    added get single post functionality

--- src/app/pages/post-page/single-post/single-post.component.ts
+++ src/app/pages/post-page/single-post/single-post.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from 'src/app/services/post/post.service';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
-  styleUrls: ['./single-post.component.scss']
+  styleUrls: ['./single-post.component.scss'],
 })
 export class SinglePostComponent implements OnInit {
+  singlePost: any;
+  errorMessage!: string;
+  isLoading = false;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.getSinglePost();
   }
 
+  getSinglePost() {
+    this.isLoading = true;
+    const postId = this.route.snapshot.params['postId'];
+
+    this.postService.getSinglePost(postId).subscribe({
+      next: (res: any) => {
+        this.isLoading = false;
+        this.singlePost = res?.data;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = err.error.message;
+      },
+    });
+  }
 }
