ed5ca9d0b92f8f33f611e7b89c0c20aa7d7c363c
Johnson Ojo <johnsonojo89@gmail.com>
2023-01-17 13:46:27 +0100

    added how it works component

---
+++ src/app/components/how-it-works/how-it-works.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface HowItWorks {
+  id: number;
+  imageUrl: string;
+  altText: string;
+  stepNumber: string;
+  details: string;
+}
+
+@Component({
+  selector: 'app-how-it-works',
+  templateUrl: './how-it-works.component.html',
+  styleUrls: ['./how-it-works.component.scss'],
+})
+export class HowItWorksComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @Input() howItWorks!: HowItWorks;
+}
