bada8dc96fcac4eb64ed93ea0d9ac3220cc155f5
Johnson Ojo <johnsonojo89@gmail.com>
2023-01-18 19:59:11 +0100

    updated homepage component

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -12,6 +12,7 @@ export class HomePageComponent implements OnInit {
       imageUrl: '../../../assets/images/login.png',
       altText: 'login',
       stepNumber: 'Step 1',
+      subTitleText: 'Register or Login',
       details:
         'Login if you have an account or register if you do not have one.',
     },
@@ -20,32 +21,45 @@ export class HomePageComponent implements OnInit {
       imageUrl: '../../../assets/images/create-button.png',
       altText: 'create',
       stepNumber: 'Step 2',
+      subTitleText: 'View all solutions',
       details:
         ' Click the create button on the top right corner to create a new post.',
     },
     {
       id: 3,
-      imageUrl: '../../../assets/images/create.png',
+      imageUrl: '../../../assets/images/create-form.png',
       altText: 'create post',
       stepNumber: 'Step 3',
+      subTitleText: 'Post a solution',
       details:
-        'Fill in the details of the problem, such as title, description and solution and click the create button. You can format your solution using the in-built text editor.',
+        'Fill in the details of the problem, such as title, description and solution(s) and click the create button.',
     },
     {
       id: 4,
       imageUrl: '../../../assets/images/search.png',
       altText: 'Search',
       stepNumber: 'Step 4',
+      subTitleText: 'Recall a solution by keyword',
       details:
-        'Navigate to the search page to search for the saved problem. Enter your in the input field, click the recall button and see a list of post matching your search keyword.',
+        'On the search page,  enter your keyword and click the recall button. Click on the "View Details" button to see the details of a problem.',
     },
     {
       id: 5,
       imageUrl: '../../../assets/images/single.png',
       altText: 'Single post',
       stepNumber: 'Step 5',
+      subTitleText: 'View a single solution',
       details:
-        'Once the desired solved problem has been found, click on the title of the post to see the details of a problem. you can choose to edit the problem details or delete it. If a problem more than one solution, you can just edit the problem and add the multiple solutions in the text editor and format it as you like.',
+        'On the post details page, you can choose to edit or delete the post.',
+    },
+    {
+      id: 6,
+      imageUrl: '../../../assets/images/edit.png',
+      altText: 'Edit',
+      stepNumber: 'Step 6',
+      subTitleText: 'Edit a solution',
+      details:
+        'You can add multiple solutions to a problem in the text editor and format it as you like.',
     },
   ];
 
